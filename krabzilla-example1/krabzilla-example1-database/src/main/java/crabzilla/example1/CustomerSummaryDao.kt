bb25e73a9e1919ebe2c498231b2f3ec5580681b9
Rodolfo <rodolfo_dpk@yahoo.com>
2017-08-12 02:31:45 -0300

    formatting code

--- krabzilla-example1/krabzilla-example1-database/src/main/java/crabzilla/example1/CustomerSummaryDao.kt
+++ krabzilla-example1/krabzilla-example1-database/src/main/java/crabzilla/example1/CustomerSummaryDao.kt
@@ -5,10 +5,12 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate
 
 interface CustomerSummaryDao {
 
-  @SqlUpdate("insert into customer_summary (id, name, is_active) values (:customerSummary.id, :customerSummary.name, false)")
+  @SqlUpdate("insert into customer_summary (id, name, is_active) values " +
+          "(:customerSummary.id, :customerSummary.name, false)")
   fun insert(customerSummary: CustomerSummary)
 
-  @SqlUpdate("update customer_summary set customer_summary.is_active = :isActive where customer_summary.id = :id")
+  @SqlUpdate("update customer_summary set customer_summary.is_active = :isActive " +
+          "where customer_summary.id = :id")
   fun updateStatus(id: String, isActive: Boolean)
 
   @SqlQuery("select id, name, is_active from customer_summary")
