4d203e1924838e897d3d3ad6ac45795db6256e88
Rodolfo <rodolfo_dpk@yahoo.com>
2017-08-13 15:03:54 -0300

    now travis should work with integration tests - 9

--- docker-entrypoint-initdb.d/create-example1-db.sql
+++ docker-entrypoint-initdb.d/create-example1-db.sql
@@ -4,10 +4,9 @@ SHOW databases ;
 
 use example1
 
-GRANT ALL PRIVILEGES ON example1.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON example1.* TO 'root'@'localhost' IDENTIFIED BY 'initial123';
 
 CREATE USER 'lula_molusco'@'localhost' IDENTIFIED BY 'initial123';
-
-GRANT ALL PRIVILEGES ON example1.* TO 'lula_molusco'@'localhost';
+GRANT ALL PRIVILEGES ON example1.* TO 'lula_molusco'@'localhost' IDENTIFIED BY 'initial123';
 
 FLUSH PRIVILEGES;
